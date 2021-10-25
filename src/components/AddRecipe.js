a484f26d48f0ce1c9eb6ca9bc04347fe32ea9013
rprabhu01 <Prabhu.N.Prabhushankar@t-mobile.com>
2021-10-25 16:16:54 -0500

    Update AddRecipe.js

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -25,7 +25,6 @@ const AddRecipe = ( {onAdd}) => {
             <div>
                 <label for="recipe-name">recipe-name</label>
                 <input type = "textbox"
-                role = "textbox"
                 value = {name}
                 onChange = { (e) => setName(e.target.value)}
                 placeholder = "recipe-name"/>
